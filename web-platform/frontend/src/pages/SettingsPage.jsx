601ce10b2b33d6fcc8381144a2149d6a23050e86
Mkuu205 <Ndegwabrian022@gmail.com>
2026-02-07 09:22:48 +0300

    Update SettingsPage.jsx

--- web-platform/frontend/src/pages/SettingsPage.jsx
+++ web-platform/frontend/src/pages/SettingsPage.jsx
@@ -1,6 +1,7 @@
 // web-platform/frontend/src/pages/SettingsPage.jsx
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import { useAuth } from '../contexts/AuthContext';
 
 function SettingsPage({ logout }) {
